40ff4552f803afc7d0d14c55d3b35ae65d9cb699
AlexandreKroll <krolla@3il.fr>
2024-04-05 14:30:33 +0200

    Création classe Task et l'interface Methode.

--- src/modele/Task.java
+++ src/modele/Task.java
@@ -1,5 +1,18 @@
 package modele;
 
 public class Task {
-
+	private int id;
+	private int titre;
+	private int description;
+	private int dateEcheance;
+	private int priorite;
+	
+	public Task(int id, int priorite) {
+		this.id = id;
+		this.titre = titre;
+		this.description = description;
+		this.dateEcheance = dateEcheance;
+		this.priorite = priorite;
+		}
+	
 }
---
+++ src/modele/interfaceMethode.java
@@ -0,0 +1,8 @@
+package modele;
+import modele.Task;
+
+public interface interfaceMethode {
+	public boolean ajouterTache(Task tache) {
+		titre.add(tache);
+	}
+}
